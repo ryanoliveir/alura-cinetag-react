da372b1502753dcd8836dfd749c2f8bdeb169fc1
ryanoliveir <ryandehallo229@gmail.com>
2023-08-19 19:47:25 -0300

    feat: List favorites in favorites page

--- src/pages/Favorites/index.jsx
+++ src/pages/Favorites/index.jsx
@@ -2,13 +2,11 @@ import styles from  './Favorites.module.css';
 import Banner from '@components/Banner'
 import Title from '@components/Title'
 import Card from '@components/Card'
+import { useFavoriteContext } from '@contexts/Favorites'
 
 const Favorites = () => {
 
-    const tempCardData = {
-        title: 'My card',
-        cover: 'https://images.unsplash.com/photo-1616530940355-351fabd9524b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80'
-    }
+    const { favorites } = useFavoriteContext();
 
     return (
         <>
@@ -17,7 +15,12 @@ const Favorites = () => {
                 <h1>Meus Favoritos</h1>
           </Title>
           <section className={styles.container}>
-            <Card {...tempCardData} />
+            {
+              favorites.map((favorite) => {
+                return <Card {...favorite} key={favorite.id}/>
+
+              })
+            }
           </section>
         </>
     )
